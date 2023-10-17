19a1dbc75ee0defc7c4e532daa8cde407af05de0
1Dhx <142033732+1Dhx@users.noreply.github.com>
2023-10-17 13:18:41 +0000

    Add files via upload

---
+++ ahu.py
@@ -0,0 +1,31 @@
+import os
+try:
+    import webbrowser
+except:
+	os.system('pip install webbrowser')
+
+print('''\033[31m
+                            _    _   _ _   _
+                           / \  | | | | | | |
+                          / _ \ | |_| | | | |
+                         / ___ \|  _  | |_| |
+                        /_/   \_\_| |_|\___/
+                   \033[32m     Arab Hackers Union
+''')  
+print('''
+
+
+
+
+
+''')
+
+print(' [ 1 ] DDos attack ')
+
+print('\033[31m good job \033[32m')
+
+webbrowser.open('https://play.google.com/store/apps/details?id=org.torproject.android')
+u = input('type ip >>> ')
+
+os.system(f'ping -s 1000 -l 3 {u}')
+	
